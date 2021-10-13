e10de2a62443e5172b1f8924c1843a6052161fa7
bowdenshot <anthonyelavezzo@gmail.com>
2021-10-12 22:31:53 -0700

    remove only text

--- public/js/header-util.js
+++ public/js/header-util.js
@@ -1,12 +1,15 @@
-const header = document.querySelector('.header')
+const header = document.querySelector('.page-header')
+const nav = document.querySelector('.nav')
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
             header.classList.add('header-disappear');
+            nav.classList.add('header-disappear')
             return;
         }
 
         header.classList.remove('header-disappear');
+        nav.classList.remove('header-disappear')
     })
   });
   
